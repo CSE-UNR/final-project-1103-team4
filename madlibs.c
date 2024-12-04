2911440fea06821854f0bbe4fccaa2885178b5a6
mspalanca <mychailapalanca@gmail.com>
2024-12-04 14:27:16 +0000

    test push

---
+++ madlibs.c
@@ -0,0 +1,11 @@
+//Author: Mychaila Palanca, Jaxson McCoy, & Jena Grant
+//Assignment: CS Final Project
+//Due Date: 12-10-2024
+
+#include <stdio.h>
+
+int main() {	
+    printf("test\n");
+    return 0;	
+}
+
